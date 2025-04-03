7ba11510bddc6382dd66d083ac2e04a2a8811460
RConradGe <rcgadduang@gmail.com>
2025-04-03 11:48:41 -0700

    wip: added sign-out functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,15 +3,25 @@ import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
-  const { user }= useContext(UserContext);
+  const { user, setUser }= useContext(UserContext);
+  
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  }
   return (
     <nav>
       <ul>
         {user ? (
-          <li>Welcome, {user.username}</li>
+          <>
+            <li>Welcome, {user.username}</li>
+            <li>
+              <Link to="/">Sign Out</Link>
+            </li>
+          </>
         ) : (
           <li>
-            <Link to="/sign-up">
+            <Link onClick={handleSignOut} to="/sign-up">
               Sign Up
             </Link>
           </li>
