d4a1c323141bc29a02b25d3a6e4dc96239c8eeb0
RConradGe <rcgadduang@gmail.com>
2025-04-03 10:55:10 -0700

    wip: provide general userContext

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+function UserProvider( { children }) { // the children prop represents each component we provide context to
+  return (
+    <UserContext.Provider>
+      { children }
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider }; // common export convention
