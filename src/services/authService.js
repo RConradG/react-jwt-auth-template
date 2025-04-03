4ff16a1d6fdc49946f8f1586941e90fc21beb73e
RConradGe <rcgadduang@gmail.com>
2025-04-03 11:34:26 -0700

    fix: fixed sign=up to sign-up in line five

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 const signUp = async (formData) => {
   try {
-    const res = await fetch (`${BASE_URL}/sign=up`, {
+    const res = await fetch (`${BASE_URL}/sign-up`, {
       method: 'Post',
       headers: {
         'Content-type': 'application/json'
