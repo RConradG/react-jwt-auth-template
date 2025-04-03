4087ee7b1e420c514be91f7bfa58bc1a4f3df623
RConradGe <rcgadduang@gmail.com>
2025-04-03 11:04:17 -0700

    wip: added UserContext, UserProvider

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,10 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
 function UserProvider( { children }) { // the children prop represents each component we provide context to
+  const [user, setUser] = useState(null);
+
+  const value = { user, setUser }; // shorthand prop syntax
+  
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value={value}>
       { children }
     </UserContext.Provider>
   );
